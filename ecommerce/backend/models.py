23f54f4eccea2e83c165d2be78b21fd339fce11f
Sevil2005 <sevillerasulova@gmail.com>
2021-02-06 00:15:24 +0400

    backend and product app created

---
+++ ecommerce/backend/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class User(AbstractUser):
+    phone = models.IntegerField(blank=True, null=True)
+    is_branch = models.BooleanField(default=True)
+    is_delivery = models.BooleanField(default=True)
+    is_phone_status = models.BooleanField(default=True)
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    address_name = models.CharField(max_length=40)
+    home = models.CharField(max_length=10)
+    city = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
+
+    #address_location
