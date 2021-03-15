5b37f4412201f49644c66cd8e7dd294a774ff5db
cavidrzayev <rzayev592@gmail.com>
2021-03-15 22:57:34 +0400

    ajax

--- ecommerce/base/urls.py
+++ ecommerce/base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('add_to_wishlist/<int:id>/',add_to_wishlist,name='add_to_wishlist'),
 ]
--- ecommerce/base/views.py
+++ ecommerce/base/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from category.models import *
 from staticpage.models import *
+from product.models import Product
 
 # Create your views here.
 
 def index(request):
     categories = Category.objects.prefetch_related('sub_categories').all()
     sliders = Slider.objects.all()
+    #request.session['test'] = 10
+    products = Product.objects.all()[:10]
     context = {
         'categories':categories,
-        'sliders':sliders 
+        'sliders':sliders,
+        'pro':products, 
     }
     return render(request,'index.html',context)
+
+def add_to_wishlist(request,id):
+    if request.method == "POST":
+        if not request.session.get('wishlist'):
+            request.session['wishlist'] = list()
+        else:
+            request.session['wishlist'] = list(request.session['wishlist'])
+        
+        items = next((item for item in request.session['wishlist'] if item['id']==id),False)
+
+
+    add_data = {
+        'id':id,
+
+    }
+    if not items:
+        request.session['wishlist'].append(add_data)
+        request.session.modifier = True
+    return redirect('index')
