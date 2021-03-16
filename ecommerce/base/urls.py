ca0d3fa6e04bc4fbe8f0b832be9d869caef43562
cavidrzayev <rzayev592@gmail.com>
2021-03-16 22:59:47 +0400

    session

--- ecommerce/base/urls.py
+++ ecommerce/base/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('add_to_wishlist/<int:id>/',add_to_wishlist,name='add_to_wishlist'),
+    path('remove_wishlist/', remove_wishlist,name='remove_wishlist')
 ]
--- ecommerce/base/views.py
+++ ecommerce/base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from category.models import *
 from staticpage.models import *
 from product.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -9,6 +10,7 @@ def index(request):
     categories = Category.objects.prefetch_related('sub_categories').all()
     sliders = Slider.objects.all()
     #request.session['test'] = 10
+    print(request.session.items())
     products = Product.objects.all()[:10]
     context = {
         'categories':categories,
@@ -23,15 +25,32 @@ def add_to_wishlist(request,id):
             request.session['wishlist'] = list()
         else:
             request.session['wishlist'] = list(request.session['wishlist'])
-        
         items = next((item for item in request.session['wishlist'] if item['id']==id),False)
-
-
     add_data = {
         'id':id,
-
     }
     if not items:
         request.session['wishlist'].append(add_data)
         request.session.modifier = True
     return redirect('index')
+
+
+def remove_wishlist(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        for i in request.session['wishlist']:
+            if str(i['id']) == id:
+                i.clear()
+                
+        while {} in request.session['wishlist']:
+            request.session['wishlist'].remove({})
+        if not request.session['wishlist']:
+            del request.session['wishlist']
+    try:
+        request.session['wishlist'] = list(request.session['wishlist'])
+    except:
+        pass
+    request.session.modifier = True
+    return JsonResponse({'status':'ok'})
+
+
