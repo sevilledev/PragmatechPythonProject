10855fdbd634806450d02125fc28d0ecba9eb44a
Sevil2005 <sevillerasulova@gmail.com>
2021-04-02 18:32:58 +0400

    cart created

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -40,6 +40,8 @@ def add_to_cart(request, id):
     return redirect('cart_products')
 
 def change_quantity(request, id, qyt):
+    id = int(id)
+    qyt = int(qyt)
     if request.method == 'POST':
         new_session_data = []
         for product in request.session['cart']:
@@ -74,7 +76,22 @@ def confirm_cart(request):
         for c in cart_products:
             cart[0].products.add(c)
             request.session['cart_id'] = cart[0].id
-    
 
-      
     return render(request, 'confirm_cart.html', context)
+
+def cart_remove(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        for i in request.session['cart']:
+            if str(i['id']) == id:
+                i.clear()
+        while {} in request.session['cart']:
+            request.session['cart'].remove({})
+        if not request.session['cart']:
+            del request.session['cart']
+    try:
+        request.session['cart'] = list(request.session['cart'])
+    except:
+        pass
+    request.session.modifier = True
+    return JsonResponse({'status':'ok'})
